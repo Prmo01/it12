d388e644e0816fc1d39a785753540e18940f5f25
Prmo01 <ronaldculas27@gmail.com>
2025-12-17 03:36:20 +0800

    revert

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,14 +11,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        // Trust all proxies (Render uses a reverse proxy)
-        $middleware->trustProxies(at: '*');
-        
-        // Force HTTPS in production
-        if (app()->environment('production')) {
-            $middleware->append(\App\Http\Middleware\ForceHttps::class);
-        }
-        
         $middleware->alias([
             'role' => \App\Http\Middleware\RoleMiddleware::class,
         ]);
